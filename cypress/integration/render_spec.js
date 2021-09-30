c0b789816b826a3c0e3ad04a0b32fdaa5bfe95ef
Daniel Vo <dan@daniel-vo.com>
2021-09-30 08:10:56 -0500

    refactor: adds first tests for rendering display

---
+++ cypress/integration/render_spec.js
@@ -0,0 +1,42 @@
+describe("rendering simple calendar", () => {
+  it("should load the calendar on demo page", () => {
+    cy.visit("http://localhost:9081");
+    cy.get("table.calendar");
+  });
+
+  it("should display the current month in the calendar header", () => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const expectedMonth = months[new Date().getMonth()];
+    cy.get("table.calendar thead tr th").contains(expectedMonth);
+  });
+
+  it("should display the current year in the calendar header", () => {
+    const expectedYear = new Date().getFullYear();
+    cy.get("table.calendar thead tr th").contains(expectedYear);
+  });
+
+  it("should display the days of the week in the calendar body", () => {
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    cy.get("table.calendar tbody tr th").each((item, index) => {
+      cy.wrap(item).should("contain.text", days[index]);
+    });
+  });
+
+  it("should display all days in the month", () => {});
+  it("should display the first day on the correct day of the week", () => {});
+  it("should display the last day on the correct day of the week", () => {});
+  it("should display empty cells for days of the week outside the current month", () => {});
+});
