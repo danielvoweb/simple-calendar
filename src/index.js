9af4e9bdea14eed6369e76da34b75b09e1382a92
Daniel Vo <dan@daniel-vo.com>
2021-09-30 07:05:07 -0500

    refactor: adds tests for calc session dates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+const today = new Date();
+const currentMonth = today.getMonth();
+const currentYear = today.getFullYear();
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
 const simpleCalendar = () => {};
 
 simpleCalendar.calculateSessionDates = (session) => {
